22d939ff9fe74facb773e87da1cb83304f798ac3
TherieI <therealtheriel@gmail.com>
2023-11-18 12:05:14 -0800

    updated react chances

--- cogs/fenns_hangouts.py
+++ cogs/fenns_hangouts.py
@@ -3,6 +3,7 @@ from discord import FFmpegPCMAudio, Message
 from asyncio import sleep
 from random import randint, random, choice
 from main import FennsBot
+from math import exp
 
 audio = "resources/vine_boom.mp3"
 BOOM_DELAY = 3.0  # vine boom has a chance of occuring every 3 seconds
@@ -15,20 +16,31 @@ class FennsHangouts(commands.Cog):
         self.bot = bot
         self.current_voice_channel = None
         self.fenns_hangouts_guild_id = 987495278892433480
-        self.react_chance = 1
+
+    def fenns_message_react_chance(self, message_len: int) -> float:
+        return 1/(8 + exp(-0.1 * message_len + 8.4)) + 0.03
 
     @commands.Cog.listener(name="on_message")
     async def fenn_react(self, message: Message):
-        if message.guild.id == self.fenns_hangouts_guild_id and random() <= self.react_chance:
-            # Fenn react
-            if message.author.id == self.bot.owner_id or random() <= self.react_chance / 5:
+        if message.guild.id == self.fenns_hangouts_guild_id:
+            react_chance = self.fenns_message_react_chance(len(message.content))
+            print(f"{react_chance=}")
+            if message.author.id == self.bot.owner_id or random() <= react_chance / 5:
                 # Give gab chad emote (or chat a small chance of these emotes)
                 emotes = ["<:nave:613525321420898339>", "<:noovin:1094798214294683718>", "<:Pog:842595542496182312>", "<:yan_left_1:843635990785949707>", "<:yan_left_2:843636001838596097>", "<:yan_right_1:843636015012773909>", "<:yan_right_2:843636023451713547>"]
             else:
-                emotes = list(message.guild.emojis) + ["💀", "💀", "💀", "💀", "<:dayruin:845485839127019520>"]
-            emote = choice(emotes)
-            await sleep(randint(3, 15))
-            await message.add_reaction(emote)
+                emotes = list(message.guild.emojis) + ["💀"] * 2
+            if len(message.attachments) >= 1 and random() <= 0.15:
+                # React to (hard) image
+                emote = choice(emotes)
+                await sleep(randint(3, 15))
+                await message.add_reaction(emote)
+                return
+            elif random() <= react_chance:
+                # Fenn react to message
+                emote = choice(emotes)
+                await sleep(randint(3, 15))
+                await message.add_reaction(emote)
 
     @commands.Cog.listener(name="on_voice_state_update")
     async def vine_boom(self, member, before, after):
